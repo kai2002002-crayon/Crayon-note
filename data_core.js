af28189c47daae6fe062078941e253d82e8d09af
kai2002002-crayon <kai2002002@gmail.com>
2026-06-16 11:26:38 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- data_core.js
+++ data_core.js
@@ -609,7 +609,7 @@ const EVENT_SCHEDULE = [
     {
         start: "2026-06-18T18:00:00+09:000",
         end: "2026-07-02T10:59:59+09:00",
-        eventId: "Theme0017" 
+        eventId: "Theme017" 
     }
 ];
 console.log("LANG_DICT 內容:", LANG_DICT);
