c2c24838d3d57fdde523928bba68420e835ae563
kai2002002-crayon <kai2002002@gmail.com>
2026-08-12 01:55:00 +0800

    Update data_core.js

--- data_core.js
+++ data_core.js
@@ -987,6 +987,12 @@ const CARD_SCHEDULE = [
 	{
         start: "2026-08-06T04:00:00+09:00",
         end: "2026-08-13T10:59:59+09:00",
+        id: "Artifact98",
+        type: "Artifact"
+    },
+	{
+        start: "2026-08-06T04:00:00+09:00",
+        end: "2026-08-13T10:59:59+09:00",
         id: "Artifact97",
         type: "Artifact"
     }
