28f76c65f4241bea06fc5f1f5ca20400dd2f9414
kai2002002-crayon <kai2002002@gmail.com>
2026-08-25 22:35:46 +0800

    Update data_aside.js

--- data_aside.js
+++ data_aside.js
@@ -727,6 +727,31 @@ const ASIDE_DATA = {
             }
         }
     },
+    "Silphir": { 
+        asideIcon: "", 
+        totalName: { zh: "", ja: "", en: "" }, 
+        stars: {
+            1: { 
+                icon: "", 
+                name: { zh: "", ja: "", en: "" }, 
+                desc: { zh: "", ja: "", en: "" }, 
+                effects: { zh: [""], ja: [""], en: [""] } 
+            },
+            2: { 
+                icon: "", 
+                name: { zh: "", ja: "", en: "" }, 
+                desc: { zh: "", ja: "", en: "" }, 
+                effects: { zh: [""], ja: [""], en: [""] } 
+            },
+            3: { 
+                icon: "", 
+                name: { zh: "", ja: "", en: "" }, 
+                desc: { zh: "", ja: "", en: "" }, 
+                effects: { zh: [""], ja: [""], en: ["%"] }, 
+                globalEffects: { zh: ["全體 %", "全體 %"], ja: ["全体 %", "全体 %"], en: ["All %", "All %"] } 
+            }
+        }
+    },
     "Sist": { 
         asideIcon: "https://i.postimg.cc/9rDrVjvj/Aside-Icon-Sist.png", 
         totalName: { zh: "名牌包", ja: "ブランドバック", en: "Luxury Bag" }, 
